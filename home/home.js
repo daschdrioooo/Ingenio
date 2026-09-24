c8fa500e873aadabe407dd7cde9df7faf3c3d35f
daschdrioooo <alikambalosman8@gmail.com>
2026-09-24 20:14:32 +0000

    updated home menu

--- home/home.js
+++ home/home.js
@@ -1,1 +1,15 @@
 lucide.createIcons();
+
+const sideBar = document.getElementById("sideBar");
+const items = document.querySelectorAll("#nav li");
+const panels = document.querySelectorAll(".panel");
+
+items.forEach(li => {
+    li.addEventListener("click", () => {
+        items.forEach(i => i.classList.remove("selected"));
+        li.classList.add("selected");
+        panels.forEach(p => p.classList.remove("active"));
+        document.getElementById(li.dataset.panel).classList.add("active");
+        sideBar.classList.add("open");
+    });
+});
